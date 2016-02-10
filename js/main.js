8646f413ec8490af68e5ab189727551739b1d35d
iPadisthin <albert.penticoss@gmail.com>
2016-02-10 22:19:15 +1100

    score

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
+    var count = 0;
    $('.up').click(function(){
       $(this).parent().css('background-color','green');
+       count += 1;
+       $('.score span').html(count);
    });
     $('.down').click(function(){
       $(this).parent().css('background-color','red'); 
+        count -= 1;
+       $('.score span').html(count);
    });
 });
