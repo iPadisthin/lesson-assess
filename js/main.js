468b22538a1064775c385aac4f631fd50b9ae1c0
iPadisthin <albert.penticoss@gmail.com>
2016-02-17 11:57:05 +1100

    not sure

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var count = 0;
-   $('.up').click(function(){
-      $(this).parent().css('background-color','green');
-       count += 1;
-       $('.score span').html(count);
-   });
-    $('.down').click(function(){
-      $(this).parent().css('background-color','red'); 
+    $('.up').click(function () {
+        $(this).parent().css('background-color', 'green');
+        count += 1;
+        $('.score span').html(count);
+    });
+    $('.down').click(function () {
+        $(this).parent().css('background-color', 'red');
         count -= 1;
-       $('.score span').html(count);
-   });
+        $('.score span').html(count);
+    });
 });
